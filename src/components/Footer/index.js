3e8ab3c51a34e68cde58faea6766bedb919da540
ciphercrypt <avimohite2019@gmail.com>
2025-04-18 23:42:00 +0530

    footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,18 @@
-import Link from 'components/Link'
 import './index.css'
 
 const Footer = () => (
     <footer className="footer">
-        <span className="footer__date">
-            {`© ${new Date().getFullYear()} Avishkar Mohite.`}
-        </span>
-        <Link secondary className="footer__link" href="/humans.txt" target="_blank">
-            humans.txt
-            
-        </Link>
+        <div className="container">
+            <p className="footer__text">
+                built by{'  '}
+                <a href="https://github.com/Ciphercrypt" target="_blank" rel="noreferrer">
+                    avishkar mohite
+                </a>
+            </p>
+            <p className="footer__sub">
+                avishkar.mohite.eng@gmail.com
+            </p>
+        </div>
     </footer>
 )
 
